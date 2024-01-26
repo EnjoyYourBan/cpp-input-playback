8805951f52d6101c894564dbfb0c043c08bfa405
EnjoyYourBan <ienjoypokemon@gmail.com>
2024-01-25 22:54:24 -0500

    release of keys is logged

--- main.cpp
+++ main.cpp
@@ -8,29 +8,29 @@
 #include <X11/Xlib.h>
 #include <X11/extensions/XTest.h>
 
-void captureInput(std::vector<KeySym>& inputs, std::vector<std::chrono::system_clock::time_point>& timepoints, Display *dis) {
+void captureInput(std::vector<int> &inputs, std::vector<std::chrono::system_clock::time_point> &timepoints, Display *dis) {
     // push initial time to compare inputs
     timepoints.push_back(std::chrono::system_clock::now());
     XEvent ev;
-    KeySym key;
+    int key;
 
     //TODO: pass this to a function, add glowy keys? 
     Window root = DefaultRootWindow(dis);
     Window window = XCreateSimpleWindow(dis, root, 0, 0, 800, 400, 1, 0, 0);
-    XSelectInput(dis, window, KeyPressMask);
+    XSelectInput(dis, window, KeyPressMask | KeyReleaseMask);
 
     XMapWindow(dis, window); // draw the window 
 
 
     do {
         XNextEvent(dis, &ev);
-        if (ev.type != KeyPress)
+        if (ev.type != KeyPress && ev.type != KeyRelease)
             continue;
 
-        key = XLookupKeysym(&ev.xkey, 0);
+        key = static_cast<int>(XLookupKeysym(&ev.xkey, 0));
         std::cout << "debug: " << key << std::endl;
 
-        inputs.push_back(key);
+        inputs.push_back(ev.type == KeyPress ? key : -key);
         timepoints.push_back(std::chrono::system_clock::now());
 
     } while (key != XK_Escape); // loop exit on escape
@@ -39,20 +39,19 @@ void captureInput(std::vector<KeySym>& inputs, std::vector<std::chrono::system_c
     
 }
 
-void playbackInput(const std::vector<uint8_t> &inputs, std::vector<int> &timepoints, Display *dis) {
+void playbackInput(const std::vector<int> &inputs, std::vector<int> &timepoints, Display *dis) {
     int timer_old = 0;
-    KeyCode key;
-    XEvent ev;
+    int key;
 
     for (size_t i = 0; i < inputs.size(); ++i) {
-        key = XKeysymToKeycode(dis, inputs[i]);
+        
+
+        
+        key = XKeysymToKeycode(dis, std::abs(inputs[i]));
         std::this_thread::sleep_for(std::chrono::milliseconds(timepoints[i] - timer_old));
+
         // Windows should use SendInput() when i care to add support
-        XTestFakeKeyEvent(dis, key, false, 0); // UNCLICK the key because this works for some reason
-        XFlush(dis);
-        XTestFakeKeyEvent(dis, key, true, 0);
-        XFlush(dis);
-        XTestFakeKeyEvent(dis, key, false, 0);
+        XTestFakeKeyEvent(dis, key, (inputs[i] > 0), 0); // UNCLICK the key because this works for some reason
         XFlush(dis);
 
         std::cout << (key) << std::endl;
@@ -61,7 +60,7 @@ void playbackInput(const std::vector<uint8_t> &inputs, std::vector<int> &timepoi
 }
 
 void makeSaveFile(std::string filePath, 
-                const std::vector<KeySym> &inputs, 
+                const std::vector<int> &inputs, 
                 const std::vector<std::chrono::system_clock::time_point>& timepoints) {
 
     std::ofstream file(filePath);
@@ -82,7 +81,7 @@ void makeSaveFile(std::string filePath,
     file.close();
 }
 
-void readPlaybackFile(std::string path, std::vector<uint8_t> &inputs, std::vector<int> &timepoints) {
+void readPlaybackFile(std::string path, std::vector<int> &inputs, std::vector<int> &timepoints) {
     std::ifstream file(path);
     if (!file.is_open()) {
         std::cout << "Failed to read file at path: "  << path << std::endl;
@@ -110,14 +109,13 @@ void readPlaybackFile(std::string path, std::vector<uint8_t> &inputs, std::vecto
 }
 
 int main(int argc, char *argv[]) {
-
+    std::vector<int> inputs;
     Display *dis;
     std::string dir;
     dis = XOpenDisplay(NULL);
     if (!dis) return 1;
 
     if (argc > 1) {
-        std::vector<uint8_t> inputs;
         std::vector<int> timestamps;
         readPlaybackFile(argv[1], inputs, timestamps);
         playbackInput(inputs, timestamps, dis);
@@ -125,8 +123,6 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-
-    std::vector<KeySym> inputs;
     std::vector<std::chrono::system_clock::time_point> timepoints;
 
     std::cout << "(ESC -> EXIT)" << std::endl;
